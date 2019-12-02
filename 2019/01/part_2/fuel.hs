542c9208279a9647dfe81aa99531196880802226
Luke Grecki <lukegrecki@gmail.com>
2019-12-02 18:44:23 -0500

    Replaced parantheses with function application
    Just for a nicer style.

--- 2019/01/part_2/fuel.hs
+++ 2019/01/part_2/fuel.hs
@@ -7,11 +7,11 @@ calculateFuelRequired :: Mass -> Fuel
 calculateFuelRequired m
     | fuelRequired > 0 = fuelRequired + calculateFuelRequired fuelRequired
     | otherwise = 0
-    where fuelRequired = floor ((fromIntegral m :: Float) / 3) - 2
+    where fuelRequired = floor $ (fromIntegral m :: Float) / 3 - 2
 
 main = do  
     contents <- getContents  
     let allLines = lines contents
-        fuelAmounts = map calculateFuelRequired (map read allLines)
+        fuelAmounts = map calculateFuelRequired $ map read allLines
         totalFuelAmount = sum fuelAmounts
     putStrLn $ show totalFuelAmount
