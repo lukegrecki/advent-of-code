fe148da2719ab58f6cbd53a7c7b0e5215284297a
Luke Grecki <lukegrecki@gmail.com>
2019-12-02 22:00:59 -0500

    Run program1 or program2 on input once

--- 2019/02/part_1/intcode.hs
+++ 2019/02/part_1/intcode.hs
@@ -3,15 +3,22 @@ import Data.List
 import Data.List.Split
 import Data.Maybe
 
--- runProgram1 :: [Int] -> [Int]
--- runProgram1 p =
---     let sum = p !! 0 + p !! 1
---         position = p !! 2
---     (take position p) ++ [sum] ++ (drop position + 1 p)
+runProgram1 :: [Int] -> [Int]
+runProgram1 p =
+    let sum = p !! 1 + p !! 2
+        position = p !! 2
+    in (take position p) ++ [sum] ++ drop (position + 1) p
+
+runProgram2 :: [Int] -> [Int]
+runProgram2 p =
+    let product = p !! 1 * p !! 2
+        position = p !! 2
+    in (take position p) ++ [product] ++ drop (position + 1) p
 
 runProgram :: [Int] -> Maybe [Int]
 runProgram p
-    | opCode == 1 = Just p
+    | opCode == 1 = Just $ runProgram1 p
+    | opCode == 2 = Just $ runProgram2 p
     | otherwise = Nothing
     where opCode = head p
 
@@ -20,5 +27,5 @@ main = do
     let program = map read (splitOn "," contents)
         output = runProgram program
     if isJust output 
-        then putStrLn $ intercalate ", " (map show (fromJust $ output))
+        then putStrLn $ intercalate ", " (map show (fromJust output))
         else return ()
