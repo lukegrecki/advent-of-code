102a35b70ce4c9b3c103c9bb687f94764833f7a1
Luke Grecki <lukegrecki@gmail.com>
2019-12-05 11:02:27 -0500

    Got a working solution for small problem sizes
    This script returns the right answers on the small test problems but runs too slowly to return on the full input.

---
+++ 2019/03/part_1/wires.hs
@@ -0,0 +1,57 @@
+import Data.Maybe
+import Data.Char
+import Data.List
+import Data.List.Split
+
+type Direction = Char
+type Magnitude = Int
+type Move = (Direction, Magnitude)
+type Path = [Move]
+type X = Int
+type Y = Int
+type Position = (X, Y)
+type Trajectory = [Position]
+type Distance = Int
+
+parseToken :: String -> Move
+parseToken (d:m) = (d, read m)
+
+parsePath :: String -> Path
+parsePath s = 
+    let tokens = splitOn "," s
+    in map parseToken tokens
+
+readPaths :: String -> [Path]
+readPaths s = map parsePath (lines s)
+
+writePath :: Path -> String
+writePath = intercalate "," . map show
+
+move :: Position -> Move -> [Position]
+move (x, y) ('U', m) = [ (x, y + i) | i <- [1,2..m] ]
+move (x, y) ('D', m) = [ (x, y - i) | i <- [1,2..m] ]
+move (x, y) ('L', m) = [ (x - i, y) | i <- [1,2..m] ]
+move (x, y) ('R', m) = [ (x + i, y) | i <- [1,2..m] ]
+
+toTrajectory :: Path -> Trajectory
+toTrajectory p = 
+    let start = [(0, 0)]
+    in tail $ concat (scanl (move . last) start p)
+
+manhattanDistance :: (Position, Position) -> Int
+manhattanDistance ((x1, y1), (x2, y2)) = abs (x1 - x2) + abs (y1 - y2)
+
+isIntersection :: (Position, Position) -> Bool
+isIntersection (p1, p2) = manhattanDistance (p1, p2) == 0
+
+findIntersections :: [Trajectory] -> [Position]
+findIntersections (t1:t2:_) = map fst (filter isIntersection [ (p1, p2) | p1 <- t1, p2 <- t2])
+
+main = do
+    contents <- getContents
+    let paths = readPaths contents
+        trajectories = map toTrajectory paths
+        intersections = findIntersections trajectories
+        zero = (0, 0)
+        smallestDistance = minimum $ map manhattanDistance $ zip (repeat zero) intersections
+    print smallestDistance
