ea86de7bc0874ad231a704ac5590acb99a72a66c
Luke Grecki <lukegrecki@gmail.com>
2019-12-07 11:12:15 -0500

    Solved part_1 of day 4
    Just a bit of recursion and filtering.

---
+++ 2019/04/part_1/password.hs
@@ -0,0 +1,25 @@
+import Data.Char
+
+digits :: Int -> [Int]
+digits = map digitToInt . show
+
+hasAdjacentDigits :: [Int] -> Bool
+hasAdjacentDigits (x1:x2:xs) = hasAdjacentDigits (x2:xs) || (x1 - x2 == 0)
+hasAdjacentDigits _ = False
+
+isMonotonic :: [Int] -> Bool
+isMonotonic (x1:x2:xs) = isMonotonic (x2:xs) && (x2 - x1 >= 0)
+isMonotonic _ = True
+
+isValidPassword :: Int -> Bool
+isValidPassword i =
+    let d = digits i
+    in hasAdjacentDigits d && isMonotonic d
+
+main = do
+    let lowerBound = 145852
+        upperBound = 616942
+        passwordRange = [lowerBound, lowerBound + 1..upperBound]
+        validPasswords = filter isValidPassword passwordRange
+
+    print $ length validPasswords
