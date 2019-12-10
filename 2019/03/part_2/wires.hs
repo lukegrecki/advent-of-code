5cba5f6158b8dcc31464b27d645b131cec418783
Luke Grecki <lukegrecki@gmail.com>
2019-12-09 19:39:22 -0500

    Got a working solution for day 3 part 2

---
+++ 2019/03/part_2/wires.hs
@@ -0,0 +1,71 @@
+import Data.Maybe
+import Data.Char
+import Data.List
+import Data.List.Split
+import qualified Data.Set as Set
+
+type Direction = Char
+type Magnitude = Int
+type Move = (Direction, Magnitude)
+type Path = [Move]
+type X = Int
+type Y = Int
+type Position = (X, Y)
+type Trajectory = [Position]
+type TrajectoryPositions = [Position]
+type SignalDelay = Int
+type PositionDelay = (Position, SignalDelay)
+type TrajectoryDelay = [PositionDelay]
+
+parseToken :: String -> Move
+parseToken (d:m) = (d, read m)
+
+parsePath :: String -> Path
+parsePath s = 
+    let tokens = splitOn "," s
+    in map parseToken tokens
+
+readPaths :: String -> [Path]
+readPaths s = map parsePath (lines s)
+
+move :: Position -> Move -> [Position]
+move (x, y) ('U', m) = [ (x, y + i) | i <- [1,2..m] ]
+move (x, y) ('D', m) = [ (x, y - i) | i <- [1,2..m] ]
+move (x, y) ('L', m) = [ (x - i, y) | i <- [1,2..m] ]
+move (x, y) ('R', m) = [ (x + i, y) | i <- [1,2..m] ]
+
+toTrajectory :: Path -> Trajectory
+toTrajectory p = 
+    let start = [(0, 0)]
+    in tail $ concat (scanl (move . last) start p)
+
+manhattanDistance :: (Position, Position) -> Int
+manhattanDistance ((x1, y1), (x2, y2)) = abs (x1 - x2) + abs (y1 - y2)
+
+isIntersection :: (Position, Position) -> Bool
+isIntersection (p1, p2) = manhattanDistance (p1, p2) == 0
+
+findShortestDelayWithin :: SignalDelay -> [TrajectoryDelay] -> Maybe SignalDelay
+findShortestDelayWithin sd (td1:td2:ts) = Just $ head [ sd1 + sd2 | (p1, sd1) <- td1, (p2, sd2) <- td2, sd1 + sd2 <= sd && isIntersection (p1, p2)]
+findShortestDelayWithin _ _ = Nothing
+
+toTrajectoryDelay :: (Trajectory, [SignalDelay]) -> TrajectoryDelay
+toTrajectoryDelay (t, sd) = zip t sd
+
+allSignalDelays :: Trajectory -> [SignalDelay]
+allSignalDelays t = [1,2..signalDelay t]
+
+signalDelay :: Trajectory -> SignalDelay
+signalDelay = length
+
+main = do
+    contents <- getContents
+    let paths = readPaths contents
+        trajectories = map toTrajectory paths
+        signalDelays = map allSignalDelays trajectories
+        longestDelay = sum $ map last signalDelays
+        trajectoryDelays = zipWith (curry toTrajectoryDelay) trajectories signalDelays
+        shortestDelay = findShortestDelayWithin longestDelay trajectoryDelays
+    case shortestDelay of
+        Just sd -> print sd
+        Nothing -> return ()
